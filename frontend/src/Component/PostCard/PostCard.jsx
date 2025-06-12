1e1e4a660268d691d3ed15375a7c3af9d03cdd9d
Mathis <mathisdumage0@gmail.com>
2025-06-12 15:51:01 +0200

    Changements de mathou

--- frontend/src/Component/PostCard/PostCard.jsx
+++ frontend/src/Component/PostCard/PostCard.jsx
@@ -3,29 +3,56 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCirclePlus, faHeart, faThumbsUp} from "@fortawesome/free-solid-svg-icons";
 import {useState} from "react";
 import {usePostRequest} from "../../Utils/Hooks/usePostRequest.js";
+import {useDeleteRequest} from "../../Utils/Hooks/useDeleteRequest.js";
 
 
 export default function PostCard({post}) {
-const [menu, setMenu] = useState(false);
-const [reactions, setReactions] = useState(post.emoticons || []);
-    const { postData } = usePostRequest(`emoticon`);
+    const [menu, setMenu] = useState(false);
+    const [reactions, setReactions] = useState(post.emoticons || []);
+    const {postData} = usePostRequest(`emoticon`);
+    const {deleteData} = useDeleteRequest(`emoticon/${post.id}`);
+    const idUser = localStorage.getItem('idUser');
 
     const addReaction = async (type) => {
         console.log(`Reaction :  ${type}`);
+        reactions.map((reaction) => {
+            if (reaction.emoticon === type && reaction.userId === idUser) {
+               deleteData(type, idUser);
+            }
+        });
         try {
-            const reaction = {
+            const newreaction = {
                 "emoticon": type,
-                "postId": post.id
+                "postId": post.id,
+                "userId": idUser
             }
 
-            await postData(reaction);
-            setReactions([...reactions, {reaction}]);
+            await postData(newreaction);
+            setReactions([...reactions, {newreaction}]);
 
         } catch (error) {
             console.error("error reactions : ", error);
         }
         setMenu(false);
     };
+
+    const deleteReaction = async (type, idUser) => {
+        console.log(`Delete :  ${type}`);
+        try {
+            const reaction = {
+                "emoticon": type,
+                "postId": post.id,
+                "authorId": idUser
+            }
+
+            await deleteData(reaction);
+            setReactions(reactions.filter((reaction) => reaction.authorId !== idUser));
+
+        } catch (error) {
+            console.error("Erreur suppression ", error);
+        }
+        setMenu(false);
+    }
 
     return (
         <div className="card">
@@ -42,31 +69,34 @@ const [reactions, setReactions] = useState(post.emoticons || []);
                     <p className="card-placeholder"></p>
                 )}
                 <span className="card-emoticons">
-                    {reactions && post.emoticons.length > 2 ? (
-                        <span>{post.emoticons.length} personnes ont réagis à ce post</span>
-                    ) : reactions && post.emoticons[0]?.type === "like" ? (
-                        <div>1 <FontAwesomeIcon icon={faThumbsUp} /></div>
-                    ) : reactions && post.emoticons[0]?.type === "love" ? (
-                        <div>1 <FontAwesomeIcon icon={faHeart} /></div>
-                    ) : null}
-                    <FontAwesomeIcon className="reaction-button" onClick={()=>{
-                        setMenu(!menu);
-                    }} icon={faCirclePlus} />
+                {reactions && post.emoticons.length > 1 ? (
+                    <span>{post.emoticons.length} personnes ont réagis à ce post</span>
+                ) : reactions && post.emoticons[0]?.type === "like" ? (
+                    <div>1 <FontAwesomeIcon icon={faThumbsUp}/></div>
+                ) : reactions && post.emoticons[0]?.type === "love" ? (
+                    <div>1 <FontAwesomeIcon icon={faHeart}/></div>
+                ) : null}
+
+
+                    <FontAwesomeIcon
+                        className="reaction-button"
+                        onClick={() => {
+                            setMenu(!menu);
+                        }}
+                        icon={faCirclePlus}
+                    />
                     {menu && (
                         <div className="reaction-menu">
                             <button onClick={() => addReaction("like")}>
-                                <FontAwesomeIcon icon={faThumbsUp} />
+                                <FontAwesomeIcon icon={faThumbsUp}/>
                             </button>
                             <button onClick={() => addReaction("love")}>
-                                <FontAwesomeIcon icon={faHeart} />
+                                <FontAwesomeIcon icon={faHeart}/>
                             </button>
                         </div>
                     )}
-                </span>
+            </span>
             </div>
         </div>
-    )
+    );
 }
-
-
-
